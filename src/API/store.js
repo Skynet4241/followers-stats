826c860f0c84d8b12ff092abecab03ae06016281
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-06-24 15:04:24 +0300

    update API

--- src/API/store.js
+++ src/API/store.js
@@ -1,37 +1,12 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { usersInfoApi } from "./getUsersInfo";
-import { userInfoSlice } from "./userSlice";
-
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-const rootReducer = combineReducers({
-  [usersInfoApi.reducerPath]: usersInfoApi.reducer,
-  currentUser: persistReducer(persistConfig, userInfoSlice.reducer),
-});
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(usersInfoApi.middleware),
-});
+  reducer: {
+    [usersInfoApi.reducerPath]: usersInfoApi.reducer,
+  },
 
-export let persistor = persistStore(store);
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(usersInfoApi.middleware),
+});
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,12 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { persistor, store } from "./API/store";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./API/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter basename="followers-stats">
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </BrowserRouter>
   </Provider>
 );
